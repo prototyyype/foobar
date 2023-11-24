3a28bb9e33a103869f0c915a7b41ece355f03ac5
Carly Gordon <carlygordon97@gmail.com>
2023-11-23 17:56:00 -0800

    submitted file

--- second/level2/coded_messages.py
+++ second/level2/coded_messages.py
@@ -1,2 +1,14 @@
 def solution(l, t):
-    # Your code here
+    left, total = 0,0
+
+    for right, num in enumerate(l):
+        total += num
+
+        while total > t:
+            total -= l[left]
+            left += 1
+
+        if total == t:
+            return [left, right]
+
+    return [-1, -1]
